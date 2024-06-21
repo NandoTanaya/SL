e1dec3ceda4f05553ad3c6be14214e91da3574a2
Dewa Bagas Pratama <150209732+KorozaX@users.noreply.github.com>
2024-06-21 22:16:09 +0700

    Update build.gradle.kts
    Efficiency changes for build

--- SignLingo/app/build.gradle.kts
+++ SignLingo/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-
+    id("kotlin-android")
 }
 
 android {
@@ -19,7 +19,7 @@ android {
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -31,15 +31,16 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
-
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("androidx.appcompat:appcompat:1.7.0")
-
+    implementation("androidx.appcompat:appcompat:1.7.0")
 }
